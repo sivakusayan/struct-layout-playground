c0829fbd61ea34a1469eb257386c23c0282803a1
Sayan Sivakumaran <sivakusayan@gmail.com>
2025-04-06 15:30:20 -0500

    Add signature for optimizer function

---
+++ src/struct-analyzer/analyzer.ts
@@ -0,0 +1,54 @@
+type Type = PrimitiveType | PointerType | ArrayType | StructureType;
+const enum TypeKind {
+    PRIMITIVE, POINTER, ARRAY, STRUCTURE
+}
+type PrimitiveType = {
+    kind: TypeKind.PRIMITIVE;
+    typeId: number;
+};
+type PointerType = {
+    kind: TypeKind.POINTER;
+    pointeeType: Type;
+};
+type ArrayType = {
+    kind: TypeKind.ARRAY;
+    length: number;
+    arrayItemType: Type;
+};
+type StructureType = {
+    kind: TypeKind.STRUCTURE;
+    structure: Structure;
+};
+type StructureMember = {
+    name: string;
+    type: Type;
+    alignmentOverride: number | null;
+};
+type Structure = {
+    name: string | null;
+    members: StructureMember[];
+};
+
+const enum AnalyzeErrorCode {
+    INVALID_CODE,
+    MAX_DEPTH_REACHED
+}
+type AnalyzeError = {
+    error: AnalyzeErrorCode;
+    errorMessage: string | null;
+    line: number | null;
+}
+type AnalyzeResult = {
+    optimizedStructure: Structure;
+    optimizedDataSize: number;
+    optimizedSize: number;
+}
+
+export const analyzer = (code: string): AnalyzeResult | AnalyzeError => {
+    console.log(code);
+    return {
+        error: AnalyzeErrorCode.INVALID_CODE,
+        errorMessage: null,
+        line: null
+    };
+} 
